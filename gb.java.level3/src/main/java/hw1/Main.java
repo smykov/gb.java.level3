d218845d6dec2065219b8a266488857f2a032b24
Konstantin <smykovkn@gmail.com>
2022-08-22 21:31:33 +0700

    Второе задание. Функция преброзовывающая массиы в лист

--- gb.java.level3/src/main/java/hw1/Main.java
+++ gb.java.level3/src/main/java/hw1/Main.java
@@ -1,16 +1,31 @@
 package hw1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int[] result = swap(array, 4, 8);
+        Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Integer[] result = swap(array, 4, 8);
         System.out.println(Arrays.toString(result));
+
+        List resultList = new ArrayList<>();
+        resultList = getArrayList(array);
+
+
     }
 
-    private static int[] swap(int[] array, int firstIndex, int secondIndex) {
+    private static List getArrayList(Integer[] array) {
+        List<Integer> arrayList = new ArrayList<>();
+        Collections.addAll(arrayList, array);
+
+        return arrayList;
+    }
+
+    private static Integer[] swap(Integer[] array, int firstIndex, int secondIndex) {
         int firstIndexValue = array[firstIndex];
         array[firstIndex] = array[secondIndex];
         array[secondIndex] = firstIndexValue;
