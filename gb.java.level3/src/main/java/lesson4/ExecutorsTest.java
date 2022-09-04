efdc663a474a202550308a7708c7e2795a9ab2fa
Konstantin <smykovkn@gmail.com>
2022-09-04 17:28:09 +0700

    newCachedThreadPool потоки создаются новые когда все имеющиеся заняты

--- gb.java.level3/src/main/java/lesson4/ExecutorsTest.java
+++ gb.java.level3/src/main/java/lesson4/ExecutorsTest.java
@@ -6,12 +6,36 @@ import java.util.concurrent.Executors;
 public class ExecutorsTest {
     public static void main(String[] args) {
 
-        ExecutorService treadManager = Executors.newFixedThreadPool(4);
-        for (int i = 0; i < 10; i++) {
+        //фиксированое количество потоков, и обработка в с ожиданием освобождения потока
+        fixedThreadPool(4);
+        //потоки создаются новые когда все имеющиеся заняты
+        cachedThreadPool();
+
+    }
+
+    private static void fixedThreadPool(int nThreads) {
+        ExecutorService treadManager = Executors.newFixedThreadPool(nThreads);
+        for (int i = 1; i <= 10; i++) {
             int k = i;
             treadManager.submit(() -> {
                 //task
                 System.out.println(Thread.currentThread().getName() + ": " + k);
+            });
+        }
+    }
+
+    private static void cachedThreadPool() {
+        ExecutorService treadManager = Executors.newCachedThreadPool();
+        for (int i = 1; i <= 10; i++) {
+            int k = i;
+            treadManager.submit(() -> {
+                //task
+                System.out.println(Thread.currentThread().getName() + ": " + k);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
             });
         }
     }
