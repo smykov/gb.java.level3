c3d14cc0cd289019bb7da8e8464f766cd341e48e
Konstantin <smykovkn@gmail.com>
2022-09-04 09:26:42 +0700

    пример: потоки без синхронизации

---
+++ gb.java.level3/src/main/java/lesson4/Store.java
@@ -0,0 +1,36 @@
+package lesson4;
+
+public class Store {
+    private int product = 0;
+
+    public void getProduct() {
+        product--;
+        System.out.println("Покупатель купил 1 товар.");
+        System.out.println("Всего товаров осталось: " + product);
+    }
+
+    public void setProduct() {
+        product++;
+        System.out.println("Продавец пополнил запасы на 1 товар.");
+        System.out.println("Всего товаров осталось: " + product);
+    }
+
+}
+
+class MainStore {
+    public static void main(String[] args) {
+        Store store = new Store();
+        //покупатель
+        new Thread(() -> {
+            for (int i = 0; i < 15; i++) {
+                store.setProduct();
+            }
+        }).start();
+        //продавец
+        new Thread(() -> {
+            for (int i = 0; i < 15; i++) {
+                store.getProduct();
+            }
+        }).start();
+    }
+}
